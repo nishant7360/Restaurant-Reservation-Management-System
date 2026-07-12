9e0b27a1c924f3eedc1a369b6a6c6219f8d6c718
Nishant Yadav <nishanty736@gmail.com>
2026-07-12 09:09:28 +0530

    update: cors

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://restaurant-reservation-management-s-lovat.vercel.app",
+    ],
     credentials: true,
   }),
 );
